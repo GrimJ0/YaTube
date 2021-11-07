1fb003e682ead5243d20605ef44925b203919e42
Vardan <kinlord@mail.ru>
2021-11-08 02:05:34 +0300

    -add verbose_name -add help_text

--- posts/models.py
+++ posts/models.py
@@ -6,9 +6,11 @@ User = get_user_model()
 
 class Group(models.Model):
 
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField(verbose_name='Название сообщества', max_length=100)
+    slug = models.SlugField(verbose_name='Адрес',
+                            help_text='Задайте уникальный адрес сообщества',
+                            unique=True)
+    description = models.TextField(verbose_name='Описание', help_text='Опишите группу')
 
     def __str__(self):
         return self.title
@@ -16,35 +18,52 @@ class Group(models.Model):
 
 class Post(models.Model):
 
-    group = models.ForeignKey(Group, blank=True, null=True, related_name="posts", on_delete=models.SET_NULL)
-    title = models.CharField(max_length=100, unique=True)
-    text = models.TextField()
-    pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    group = models.ForeignKey(
+                            Group, blank=True, null=True,
+                            related_name="posts",
+                            on_delete=models.SET_NULL,
+                            verbose_name='Сообщество',
+                            help_text='Выберите группу'
+                            )
+    title = models.CharField(verbose_name='Заголовок', max_length=100, unique=True)
+    text = models.TextField(verbose_name='Текст поста', help_text='Введите текст вашего поста')
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts", verbose_name='Автор')
+    image = models.ImageField(upload_to='posts/', blank=True, null=True,
+                              verbose_name='Изображение',
+                              help_text='Добавьте изображение к посту'
+                              )
 
     def __str__(self):
         return self.text
 
     class Meta:
         ordering = ('-pub_date',)
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
 
 
 class Comment(models.Model):
 
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
-    text = models.TextField()
-    created = models.DateTimeField("date published", auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments", verbose_name='Пост')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments", verbose_name='Автор')
+    text = models.TextField(verbose_name='Комментария', help_text='Введите ваш комментарий')
+    created = models.DateTimeField("Дата публикации", auto_now_add=True)
 
     def __str__(self):
         return self.text
 
     class Meta:
         ordering = ('-created',)
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
 
 
 class Follow(models.Model):
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower", verbose_name='Подписчик')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following", verbose_name='Автор')
+
+    class Meta:
+        verbose_name = 'Подписчик'
+        verbose_name_plural = 'Подписчики'
